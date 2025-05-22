2494db058e1dd56f064d3967dd86afe8a754c62d
loofxtrt <loofxtrt@gmail.com>
2025-05-21 22:44:57 -0300

    functions

--- arch_based_distros.sh
+++ arch_based_distros.sh
@@ -3,69 +3,97 @@
 set -e # parar caso algum erro aconteça
 sudo pacman -Syu # atualizar o sistema e as packages pela primeira vez
 
-# --- PRÉ-REQUISITOS ---
-# base-devel: grupo de pacotes com ferramentas básicas para compilar programas (inclui make, gcc, binutils, pkgconf, etc.) muitas aur packages exigem isso
-# git: controle de versão
-# wget: ferramenta de linha de comando para baixar arquivos da internet
-# unzip: utilitário para descompactar arquivos .zip
-# ripgrep: ferramenta de busca baseada em grep, ex: procurar pela string "lorem_ipsum" em todos os arquivos recursivamente a partir do diretório atual
-# make:
-# make: ferramenta de automação de compilação (já vem no base-devel, mas só pra garantir)
-# gcc: compilador C/C++ (também parte do base-devel)
-# --needed faz com o que pacman instale apenas os softwares que ainda NÃO existem no sistema, evitando reinstalações desnecessárias
-sudo pacman -S base-devel git wget unzip ripgrep make gcc --needed
+install_basics() {
+    echo "Now installing basic software"
 
-# --- FLATPAK ---
-sudo pacman -S flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo # adicionar o repositório do flatpak ao sistema
+    # base-devel: grupo de pacotes com ferramentas básicas para compilar programas (inclui make, gcc, binutils, pkgconf, etc.) muitas aur packages exigem isso
+    # git: controle de versão
+    # wget: ferramenta de linha de comando para baixar arquivos da internet
+    # unzip: utilitário para descompactar arquivos .zip
+    # ripgrep: ferramenta de busca baseada em grep, ex: procurar pela string "lorem_ipsum" em todos os arquivos recursivamente a partir do diretório atual
+    # make:
+    # make: ferramenta de automação de compilação (já vem no base-devel, mas só pra garantir)
+    # gcc: compilador C/C++ (também parte do base-devel)
+    # --needed faz com o que pacman instale apenas os softwares que ainda NÃO existem no sistema, evitando reinstalações desnecessárias
+    sudo pacman -S base-devel git wget unzip ripgrep make gcc --needed
+}
 
-# --- SOFTWARE VIA FLATPAK ---
-flatpak install -y flathub \
-    org.kde.kdenlive \
-    com.kristianduske.TrenchBroom \
-    org.sqlitebrowser.sqlitebrowser \
-    net.blockbench.Blockbench \
-    net.lutris.Lutris \
-    com.github.tchx84.Flatseal \
-    com.valvesoftware.Steam \
-    com.usebottles.bottles \
-    com.github.beeRef.BeeRef
+install_managers() {
+    install_flatpak() {
+        echo "Now installing packman"
 
-# --- SOFTWARE VIA PACMAN ---
-sudo pacman -S \
-    firefox \
-    audacity \
-    krita \
-    inkscape \
-    qt5-tools \
-    neovim \
-    kvantum \
-    blender \
-    godot \
-    gimp \
-    obsidian \
-    discord \
-    spotify-launcher \
-    obs-studio \
-    wine \
-    openjdk21 \
-    python \
-    lua
-    --needed
-
-# --- YAY ---
-# testar se o comando yay já existe ou não
-# se não exsitr, clona o repositório do yay, entra no diretório novo, compila e depois volta
-if ! command -v yay &> /dev/null; then
-  git clone https://aur.archlinux.org/yay.git
-  cd yay
-  makepkg -si
-  cd ..
-fi
-
-# --- SOFTWARE VIA YAY (AUR) ---
-yay -S \
-    btop \
-    vscodium-bin \
-    libresprite
+        sudo pacman -S flatpak
+        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo # adicionar o repositório do flatpak ao sistema
+    }
     
+    install_yay() {
+        # testar se o comando yay já existe ou não
+        # se não exsitr, clona o repositório do yay, entra no diretório novo, compila e depois volta
+        if ! command -v yay &> /dev/null; then
+            echo "Now installing yay"
+
+            git clone https://aur.archlinux.org/yay.git
+            cd yay
+            makepkg -si
+            cd ..
+        fi
+    }
+
+    install_flatpak
+    install_yay
+}
+
+install_software_via_flatpak() {
+    echo "Now installing software via flatpak"
+
+    flatpak install -y flathub \
+        org.kde.kdenlive \
+        com.kristianduske.TrenchBroom \
+        org.sqlitebrowser.sqlitebrowser \
+        net.blockbench.Blockbench \
+        net.lutris.Lutris \
+        com.github.tchx84.Flatseal \
+        com.valvesoftware.Steam \
+        com.usebottles.bottles \
+        com.github.beeRef.BeeRef
+}
+
+install_software_via_pacman() {
+    echo "Now installing software via pacman"
+
+    sudo pacman -S \
+        firefox \
+        audacity \
+        krita \
+        inkscape \
+        qt5-tools \
+        neovim \
+        kvantum \
+        blender \
+        godot \
+        gimp \
+        obsidian \
+        discord \
+        spotify-launcher \
+        obs-studio \
+        wine \
+        openjdk21 \
+        python \
+        lua
+        --needed
+}
+
+install_software_via_yay() {
+    echo "Now installing software via yay"
+
+    yay -S \
+        btop \
+        vscodium-bin \
+        libresprite
+}
+
+install_basics
+install_managers
+install_software_via_flatpak
+install_software_via_pacman
+install_software_via_yay
